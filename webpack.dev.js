177dc8c939c5b6dc75e2d67a54dafe40e3e55536
hikmanisyariful <syarifulfajar017@gmail.com>
2021-11-26 20:58:02 +0700

    Set up clean-webpack-plugin in webpack config development

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -18,6 +19,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html"
-    })
+    }),
+    new CleanWebpackPlugin()
   ]
 };
