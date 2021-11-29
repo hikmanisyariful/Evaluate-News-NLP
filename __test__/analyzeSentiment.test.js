dd83eb29b1e5a78ac4527296818ac5ae17247939
hikmanisyariful <syarifulfajar017@gmail.com>
2021-11-29 18:48:58 +0700

    4 test passed

--- __test__/analyzeSentiment.test.js
+++ __test__/analyzeSentiment.test.js
@@ -1,8 +1,30 @@
+require("dotenv").config();
 import "regenerator-runtime/runtime";
-var FormData = require("form-data");
+import fetch from "cross-fetch";
 import { analyzeSentiment } from "../src/client/js/analyzeSentiment.js";
+var FormData = require("form-data");
 
 describe("Sentiment Analysis API", () => {
+  test("It should return data json", async () => {
+    const formData = new FormData();
+    formData.append("key", process.env.API_KEY);
+    formData.append("txt", "I like jest");
+    formData.append("lang", "en");
+    const requestOptions = {
+      method: "POST",
+      body: formData,
+      redirect: "follow"
+    };
+
+    const response = await fetch(
+      "https://api.meaningcloud.com/sentiment-2.1",
+      requestOptions
+    );
+    const json = await response.json();
+    expect(json).toHaveProperty("score_tag");
+    expect(json).toHaveProperty("subjectivity");
+  });
+
   test("It should return error", async () => {
     const formData = new FormData();
     formData.append("key", "wrongapikey");
--- __test__/getApiKey.test.js
+++ __test__/getApiKey.test.js
@@ -1,7 +1,16 @@
+require("dotenv").config();
+import fetch from "cross-fetch";
 import "regenerator-runtime/runtime";
 import { getApiKey } from "../src/client/js/getApiKey.js";
 
 describe("GET API KEY", () => {
+  test("It should return api key", async () => {
+    const response = await fetch("http://localhost:8081/api");
+    const json = await response.json();
+    const apiKey = json.apiKey;
+    expect(apiKey).toBe(process.env.API_KEY);
+  });
+
   test("It should return error", async () => {
     try {
       await getApiKey();
